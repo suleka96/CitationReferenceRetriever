d5309a6c72fb8b2e3d2a6bd9cc6a50362206c042
Suleka Helmini <sulekahelmini96comp@gmail.com>
2019-03-11 12:32:36 +0530

    Do adjustments to the code
    1. when taking the input, get them with space between two DOIs 
    2. print paper title to the screen 
    3. put citation data into one file, not many files. Use paper ID, not the title.

--- InfoRetrieve_v1.py
+++ InfoRetrieve_v1.py
@@ -21,8 +21,9 @@ def write_paperInfo(paperId,title,url,year,venue,citationVelocity,influentialCit
             zip([paperId], [title], [url], [year], [venue],
                 [citationVelocity], [influentialCitationCount], [citation_count]))
 
-def write_citedReferencedInfo(fileName,paper,citedPaper):
-    with open(fileName, "a", errors='replace') as f:
+def write_citedReferencedInfo(paper,citedPaper):
+    fileName_cited_referenced_graph = '{}{}'.format(filePath, "/papers/citedReferenced.csv")
+    with open(fileName_cited_referenced_graph, "a", errors='replace') as f:
         writer = csv.writer(f)
         writer.writerows(
             zip([paper], [citedPaper]))
@@ -79,81 +80,82 @@ def getInfo(doi_id):
         response = requests.get(url)
         response_native = json.loads(response.text)
 
-        id = response_native.get('paperId')
-
-        if id in all_papers:
+        if response_native.get('error') == "Paper not found":
             continue
-
         else:
+            id = response_native.get('paperId')
 
-            title = response_native.get('title')
-            url = response_native.get('url')
-            year = response_native.get('year')
-            venue = response_native.get('venue')
-            citationVelocity = response_native.get('citationVelocity')
-            influentialCitationCount = response_native.get('influentialCitationCount')
+            if id in all_papers:
+                continue
 
-            paper_name_year = title + " - " + str(year)
+            else:
 
-            all_papers[id]= title
+                title = response_native.get('title')
+                url = response_native.get('url')
+                year = response_native.get('year')
+                venue = response_native.get('venue')
+                citationVelocity = response_native.get('citationVelocity')
+                influentialCitationCount = response_native.get('influentialCitationCount')
 
-            fileName_cited_referenced_graph = '{}{}{}{}{}'.format(filePath,"/papers/paper_", id, "_CitedReferenced", ".csv")
-            fileName_json = '{}{}{}{}'.format(filePath,"/json/paper_", id, ".json")
+                paper_id_year = id + " - " + str(year)
 
+                all_papers[id]= title
 
-            write_citedReferencedInfo(fileName_cited_referenced_graph,"Paper","Cited Paper")
+                fileName_json = '{}{}{}{}'.format(filePath,"/json/paper_", id, ".json")
 
-            with open(fileName_json, 'w') as outfile:
-                json.dump(response_native, outfile)
+                with open(fileName_json, 'w') as outfile:
+                    json.dump(response_native, outfile)
 
-            citation_count = 0
-            citations = response_native.get('citations')
-            for citation in citations:
-                citation_id = citation.get('paperId')
+                citation_count = 0
+                citations = response_native.get('citations')
+                for citation in citations:
+                    citation_id = citation.get('paperId')
 
-                if citation_id in citedReferencedPapers:
-                    continue
+                    if citation_id in citedReferencedPapers:
+                        continue
 
-                else:
-                    citation_count += 1
+                    else:
+                        citation_count += 1
 
-                    citation_title = citation.get('title')
-                    citation_year = citation.get('year')
+                        citation_title = citation.get('title')
+                        citation_year = citation.get('year')
 
-                    citedReferencedPapers[citation_id] = citation_title
+                        citedReferencedPapers[citation_id] = citation_title
 
-                    temp_paper_list.append(citation_id)
+                        temp_paper_list.append(citation_id)
 
-                    citedPaper_name_year = citation_title + " - " + str(citation_year)
+                        citedPaper_id_year = citation_id + " - " + str(citation_year)
 
-                    write_citedReferencedInfo(fileName_cited_referenced_graph, paper_name_year, citedPaper_name_year)
+                        write_citedReferencedInfo(paper_id_year, citedPaper_id_year)
 
-            write_paperInfo(id, title, url, year, venue, citationVelocity, influentialCitationCount,citation_count)
+                write_paperInfo(id, title, url, year, venue, citationVelocity, influentialCitationCount,citation_count)
 
-            references = response_native.get('references')
-            for reference in references:
-                reference_id = reference.get('paperId')
+                references = response_native.get('references')
+                for reference in references:
+                    reference_id = reference.get('paperId')
 
-                if reference_id in citedReferencedPapers:
-                    continue
+                    if reference_id in citedReferencedPapers:
+                        continue
 
-                else:
+                    else:
 
-                    reference_title = reference.get('title')
-                    reference_year = reference.get('year')
+                        reference_title = reference.get('title')
+                        reference_year = reference.get('year')
 
-                    citedReferencedPapers[reference_id] = reference_title
+                        citedReferencedPapers[reference_id] = reference_title
 
-                    temp_paper_list.append(reference_id)
+                        temp_paper_list.append(reference_id)
 
-                    referencedPaper_name_year = reference_title + " - " + str(reference_year)
+                        referencedPaper_id_year = reference_id + " - " + str(reference_year)
 
-                    write_citedReferencedInfo(fileName_cited_referenced_graph, referencedPaper_name_year, paper_name_year)
+                        write_citedReferencedInfo(referencedPaper_id_year, paper_id_year)
 
-            if repeat:
-                repeat = False
-                getInfo(temp_paper_list)
-                repeat = True
+                print("Retrieved Information of: " + title)
+
+                if repeat:
+                    repeat = False
+                    getInfo(temp_paper_list)
+                    repeat = True
 
 
 if __name__ == '__main__':
@@ -169,20 +171,20 @@ if __name__ == '__main__':
     createDirectries()
 
     write_paperInfo("Paper Id","Title","URL","Year","Venue","CitationVelocity","InfluentialCitationCount","TotalCitationCount")
+    write_citedReferencedInfo("Paper", "Cited Paper")
 
     doi = []
 
     print("\n"+"*********************************"+ "\n")
-    print("To enter DOIs, type in the DOI and press 'Enter'." + "\n"
-          "Reapeat process and when done, simply press the 'Enter' key again."+ "\n")
+    print("To enter DOIs, type in the DOI and press 'space bar'." + "\n"
+          "Reapeat process and when done, simply press the 'Enter' key."+ "\n")
     print("*********************************" + "\n")
-    print( "Enter DOIs" )
 
-    while True:
-        line = input()
-        if not line: break
-        doi.append(line)
+    input = input("Please enter DOIs: ")
+
+    doi = [i for i in input.split()]
 
     print("Retrieving Information... ")
 
     getInfo(doi)
+
